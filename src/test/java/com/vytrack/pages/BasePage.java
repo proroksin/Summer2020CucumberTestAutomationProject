a88fe460a20b5dfb9be8c86126f6eb2a4cfd5fe7
proroksin <sinaverkemalov@gmail.com>
2020-10-24 14:38:16 -0500

    new

---
+++ src/test/java/com/vytrack/pages/BasePage.java
@@ -0,0 +1,43 @@
+package com.vytrack.pages;
+
+import com.vytrack.utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class BasePage {
+
+    @FindBy(className = "oro-subtitle")
+    protected WebElement pageSubTitle;
+
+    public BasePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    public String getPageSubTitleText() {
+        return pageSubTitle.getText();
+    }
+
+    /**
+     * Method for navigation in vytrack app
+     * @param tab, for example : Fleet, Dashboard, Sales, Activities..
+     * @param module, one of the values that will be visible after cklicking on the tab
+     * For Fleet: there are the modules: Vehicles, Vehicle Odometer, Vehicle Costs, etc...
+     */
+
+    public void navigateTo(String tab, String module){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),30);
+        String tabXpath = "//*[contains(text()," + tab + "') and @class='title title-level-1']";
+        String moduleXpath = "//*[contains(text()," + module + "') and @class='title title-level-2']";
+
+        WebElement tabElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(tabXpath)));
+        wait.until(ExpectedConditions.elementToBeClickable(tabElement)).click();
+
+        WebElement moduleElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(moduleXpath)));
+        wait.until(ExpectedConditions.elementToBeClickable(moduleElement)).click();
+
+    }
+}
