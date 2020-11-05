5b4ee05ba91346768de461d8cf60c0bfe15dcecc
proroksin <sinaverkemalov@gmail.com>
2020-11-04 19:52:07 -0600

    up

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -31,6 +31,12 @@ public class LoginStepDefinitions {
         Driver.closeDriver();
     }
 
+    @Then("user should see {string} page")
+    public void user_should_see_page(String string) {
+        String actual = loginPage.getPageSubTitleText().trim();
+        Assert.assertEquals("Page title is not correct!", string, actual);
+    }
+
     //When user logs in as a "driver" --> public void user_logs_in_as_a(String string) -> loginPage.login(string); -> public void login(String role) { if role == "" do this..}
     //When user logs in as a "sales manager"
     //When user logs in as a "store manager"
@@ -39,19 +45,20 @@ public class LoginStepDefinitions {
         loginPage.login(string);
     }
 
-    //String string = "storemanager85"
-    //String string2 = "wrong"
+    //  String string = "storemanager85";
+//  String string2 =  "wrong";
     //When user logs in with "storemanager85" username and "wrong" password
     @When("user logs in with {string} username and {string} password")
     public void user_logs_in_with_username_and_password(String string, String string2) {
         loginPage.login(string, string2);
     }
 
-    //Then user verifies that "Invalid user name or password." message is displayed
+    //    String expected = "Invalid user name or password."
+//    Then user verifies that "Invalid user name or password." message is displayed
     @Then("user verifies that {string} message is displayed")
     public void user_verifies_that_message_is_displayed(String expected) {
         String actualResult = loginPage.getWarningMessageText();
-        Assert.assertEquals(expected,actualResult);
+        Assert.assertEquals(expected, actualResult);
     }
 
 }
